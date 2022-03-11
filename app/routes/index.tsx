28aee8bbb6c8ed4e42727a4cf57d21c5f4b304e7
Tim Officer <timothyofficer@gmail.com>
2022-03-11 16:02:39 -0500

    init 2

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,59 @@
+import { Fragment } from 'react'
+import styled from 'styled-components'
+import Typography from '~/components/Typography'
+
+const Card = styled.div`
+  position: relative;
+  width: 800px;
+  margin: 100px auto 0;
+  border-radius: 50px;
+`
+
+const CardHeader = styled.div`
+  text-align: center;
+  background-color: red;
+  padding: 10px 0;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+`
+
+const CardHeaderTopText = styled(Typography)`
+  font-size: 60px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: white;
+`
+
+const CardHeaderBottomText = styled(Typography)`
+  font-size: 26px;
+  margin-top: --10px;
+  color: white;
+`
+
+const CardBodyText = styled(Typography)`
+  text-align: center;
+  font-size: 180px;
+  padding: 30px 0;
+`
+
+const CardFooter = styled.div`
+  height: 35px;
+  background: red;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+`
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <Fragment>
+      <Card>
+        <CardHeader>
+          <CardHeaderTopText>Hello</CardHeaderTopText>
+          <CardHeaderBottomText>my name is</CardHeaderBottomText>
+        </CardHeader>
+        <CardBodyText variant="cursive">Tim</CardBodyText>
+        <CardFooter></CardFooter>
+      </Card>
+    </Fragment>
+  )
 }
